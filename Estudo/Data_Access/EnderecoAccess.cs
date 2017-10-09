8631f5318273e4bf4a354f0bbe7a8721fcf3a71a
Patrik Muller <patrik.muller@hotmail.com>
2017-10-09 12:18:45 -0400

    Módulo de Endereço.

---
+++ Estudo/Data_Access/EnderecoAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Estudo.Data_Access
+{
+    public class EnderecoAccess
+    {
+    }
+}
---
+++ Estudo/Models/Endereco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Estudo.Models
+{
+    public class Endereco
+    {
+        public virtual int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor preencha o campo.")]
+        [StringLength(200, ErrorMessage = "Digite no máximo 200 caracteres.")]
+        public virtual string Descricao { get; set; }
+
+        [Required(ErrorMessage = "por favor preencha o Conteúdo.")]
+        [StringLength(200, ErrorMessage = "Digite no máximo 200 caracteres.")]
+        public virtual string Bairro { get; set; }
+
+        [Required(ErrorMessage = "por favor preencha o Conteúdo.")]
+        [StringLength(200, ErrorMessage = "Digite no máximo 200 caracteres.")]
+        public virtual string Municipio { get; set; }
+
+        [Required(ErrorMessage = "por favor preencha o Conteúdo.")]
+        [StringLength(200, ErrorMessage = "Digite no máximo 200 caracteres.")]
+        public virtual string Cep { get; set; }
+                
+    }
+}
